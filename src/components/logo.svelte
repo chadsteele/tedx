8833d8189f0a84d0566aa73e36c7453b28d1e57d
chadsteele <chad@chadsteele.com>
2026-02-14 13:54:56 +0400

    improved logo

--- src/components/logo.svelte
+++ src/components/logo.svelte
@@ -1,59 +1,14 @@
 <script>
-	export let eventName = "ALCHE"
-	export let tagline = `<span class="red-x">x</span> = independently organized TED event`
+	export let href = "/"
 </script>
 
-<div class="tedx-container">
-	<div class="tedx-logo">
-		<span class="ted-brand">TED<span class="ted-x">x</span></span>
-		<span class="event-name">{eventName}</span>
-	</div>
-	{#if tagline}
-		<div class="tagline">
-			{@html tagline}
-		</div>
-	{/if}
-</div>
+<a {href} class="logo-link">
+	<img class="logo" src="/images/logos/logo.full.png" alt="TEDxALCHE" />
+</a>
 
 <style>
-	.tedx-container {
-		display: inline-block;
-		background-color: #000000;
-		padding: 40px;
-		font-family: Helvetica, Arial, sans-serif;
-		color: #ffffff;
-	}
-
-	.tedx-logo {
-		line-height: 1;
-		letter-spacing: -2px;
-		margin-bottom: 5px;
-		font-size: 2em;
-	}
-
-	.ted-brand {
-		color: #eb0028;
-		font-weight: bold;
-	}
-
-	.ted-x {
-		text-transform: lowercase;
-		font-weight: bold;
-	}
-
-	.event-name {
-		color: #ffffff;
-		font-weight: normal;
-	}
-
-	.tagline {
-		font-size: 0.75rem;
-		font-weight: bold;
-		letter-spacing: 0;
-	}
-
-	.red-x {
-		color: #eb0028;
-		text-transform: lowercase;
+	.logo {
+		height: 3em;
+		width: auto;
 	}
 </style>
